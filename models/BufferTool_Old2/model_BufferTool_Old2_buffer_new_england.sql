30972f85d8cb5c33df3987f2eb0b58b563cf0c9b
mesarovic.andre@prophecy.io <mesarovic.andre@prophecy.io>
2025-06-20 16:39:45 +0000

    Removed old BufferTool2 and BufferTool3 macros and related entities.

--- models/BufferTool_Old2/model_BufferTool_Old2_buffer_new_england.sql
+++ models/BufferTool_Old2/model_BufferTool_Old2_buffer_new_england.sql
@@ -1,7 +1,7 @@
 {{
   config({    
     "materialized": "table",
-    "alias": "prophecy_tmp__mc515nmt__BufferTool_Old2__buffer_new_england",
+    "alias": "prophecy_tmp__mc51b9dn__BufferTool_Old2__buffer_new_england",
     "database": "andre_dev",
     "schema": "spatial"
   })
