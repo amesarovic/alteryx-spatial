ce58fd1a46903a626a12fbf16cfdddc59e95d581
mesarovic.andre@prophecy.io <mesarovic.andre@prophecy.io>
2025-06-23 13:51:09 +0000

    BufferTool_05 macro: removed older commented snippets

--- models/BufferTool_05/model_BufferTool_05_buffer_new_england.sql
+++ models/BufferTool_05/model_BufferTool_05_buffer_new_england.sql
@@ -1,7 +1,7 @@
 {{
   config({    
     "materialized": "table",
-    "alias": "prophecy_tmp__mc94gmg4__BufferTool_05__buffer_new_england",
+    "alias": "prophecy_tmp__mc95lltd__BufferTool_05__buffer_new_england",
     "database": "andre_dev",
     "schema": "spatial"
   })
@@ -22,7 +22,7 @@ buffer_new_england AS (
       'new_england', 
       [{ "name": "name", "dataType": "String" }, { "name": "geometry", "dataType": "String" }], 
       'geometry', 
-      10, 
+      20, 
       'miles', 
       true
     )
