e5094cda5399548f8d13545348ca3155b8fe681f
mesarovic.andre@prophecy.io <mesarovic.andre@prophecy.io>
2025-06-24 06:34:28 +0000

    MakeGridTool_04 gem: updated with correct content. Still failing to run.

--- gems/MakeGridTool_04.py
+++ gems/MakeGridTool_04.py
@@ -1,26 +1,45 @@
+import dataclasses
+import json
 
-from dataclasses import dataclass
-
-
-from collections import defaultdict
-from prophecy.cb.sql.Component import *
 from prophecy.cb.sql.MacroBuilderBase import *
 from prophecy.cb.ui.uispec import *
-
 
 class MakeGridTool_04(MacroSpec):
     name: str = "MakeGridTool_04"
     projectName: str = "andre_spatial_07"
     category: str = "Spatial"
-
-
+    minNumOfInputPorts: int = 1
+    
     @dataclass(frozen=True)
     class MakeGridTool_04Properties(MacroProperties):
         # properties for the component with default values
-        parameter1: str = "'default_value_of_parameter1'"
+        relation_name: List[str] = field(default_factory=list)
+        schema: str = ''
+        cell_size: str = "0.1"
+        unit: str = "miles"
+        polygonColumnName: str = ""
+
+    def get_relation_names(self, component: Component, context: SqlContext):
+        all_upstream_nodes = []
+        for inputPort in component.ports.inputs:
+            upstreamNode = None
+            for connection in context.graph.connections:
+                if connection.targetPort == inputPort.id:
+                    upstreamNodeId = connection.source
+                    upstreamNode = context.graph.nodes.get(upstreamNodeId)
+            all_upstream_nodes.append(upstreamNode)
+
+        relation_name = []
+        for upstream_node in all_upstream_nodes:
+            if upstream_node is None or upstream_node.label is None:
+                relation_name.append("")
+            else:
+                relation_name.append(upstream_node.label)
+
+        return relation_name
 
     def dialog(self) -> Dialog:
-        return Dialog("Macro").addElement(
+        return Dialog("MakeGridTool_04").addElement(
             ColumnsLayout(gap="1rem", height="100%")
             .addColumn(
                 Ports(allowInputAddOrDelete=True),
@@ -29,32 +48,84 @@ class MakeGridTool_04(MacroSpec):
             .addColumn(
                 StackLayout()
                 .addElement(
-                    TextBox("Table Name")
-                    .bindPlaceholder("Configure table name")
-                    .bindProperty("parameter1")
-                )
+                    SchemaColumnsDropdown("Polygon Column Input")
+                        .bindSchema("component.ports.inputs[0].schema")
+                        .bindProperty("polygonColumnName")
+                )  
+                .addElement(
+                    TextBox("Cell size", placeholder="0.123", helpText="Help me").bindProperty("cell_size")
+                )                                            
+                .addElement(
+                    SelectBox("Units").addOption("Miles", "miles").addOption("Kilometers", "kms").bindProperty("unit")
+                )                                
            )
        )
 
     def validate(self, context: SqlContext, component: Component) -> List[Diagnostic]:
-        # Validate the component's state
-        return super().validate(context,component)
+        diagnostics = []
+        if len(component.properties.cell_size.strip()) == 0:
+            diagnostics.append(
+                Diagnostic(
+                    "properties.cell_size",
+                    "Field 'Cell size' cannot be empty.",
+                    SeverityLevelEnum.Error
+                )
+            )
+        else:
+            try:
+                float(component.properties.cell_size)
+            except ValueError as e:
+                diagnostics.append(
+                    Diagnostic(
+                        "properties.cell_size",
+                        "Field 'Cell size' must be a float.",
+                        SeverityLevelEnum.Error
+                    )
+                )
+        return diagnostics
+
 
     def onChange(self, context: SqlContext, oldState: Component, newState: Component) -> Component:
         # Handle changes in the component's state and return the new state
-        return newState
+        schema = json.loads(str(newState.ports.inputs[0].schema).replace("'", '"'))
+        fields_array = [{"name": field["name"], "dataType": field["dataType"]["type"]} for field in schema["fields"]]
+        relation_name = self.get_relation_names(newState, context)
+
+        newProperties = dataclasses.replace(
+            newState.properties,
+            schema=json.dumps(fields_array),
+            relation_name=relation_name
+        )
+        return newState.bindProperties(newProperties)
 
     def apply(self, props: MakeGridTool_04Properties) -> str:
-        # generate the actual macro call given the component's state
+        # Get the table name
+        table_name: str = ",".join(str(rel) for rel in props.relation_name)
+
+        # generate the actual macro call given the component's
         resolved_macro_name = f"{self.projectName}.{self.name}"
-        non_empty_param = ",".join([param for param in [props.parameter1] if param != ''])
-        return f'{{{{ {resolved_macro_name}({non_empty_param}) }}}}'
+
+        arguments = [
+            "'" + table_name + "'",
+            props.schema,
+            "'" + props.polygonColumnName + "'",            
+            str(props.cell_size),
+            "'" + props.unit + "'"
+        ]
+
+        params = ",".join([param for param in arguments])
+        return f'{{{{ {resolved_macro_name}({params}) }}}}'
+
 
     def loadProperties(self, properties: MacroProperties) -> PropertiesType:
         # load the component's state given default macro property representation
         parametersMap = self.convertToParameterMap(properties.parameters)
         return MakeGridTool_04.MakeGridTool_04Properties(
-            parameter1=parametersMap.get('parameter1')
+            relation_name=parametersMap.get('relation_name'),
+            schema=parametersMap.get('schema'),
+            polygonColumnName=parametersMap.get('polygonColumnName'),
+            cell_size=parametersMap.get('cell_size'),
+            unit=str(parametersMap.get('unit'))
         )
 
     def unloadProperties(self, properties: PropertiesType) -> MacroProperties:
@@ -63,8 +134,22 @@ class MakeGridTool_04(MacroSpec):
             macroName=self.name,
             projectName=self.projectName,
             parameters=[
-                MacroParameter("parameter1", properties.parameter1)
+                MacroParameter("relation_name", str(properties.relation_name)),
+                MacroParameter("schema", str(properties.schema)),
+                MacroParameter("destinationColumnNames", properties.polygonColumnName),
+                MacroParameter("cell_size", properties.cell_size),
+                MacroParameter("unit", properties.unit)
             ],
         )
 
+    def updateInputPortSlug(self, component: Component, context: SqlContext):
+        schema = json.loads(str(component.ports.inputs[0].schema).replace("'", '"'))
+        fields_array = [{"name": field["name"], "dataType": field["dataType"]["type"]} for field in schema["fields"]]
+        relation_name = self.get_relation_names(component, context)
 
+        newProperties = dataclasses.replace(
+            component.properties,
+            schema=json.dumps(fields_array),
+            relation_name=relation_name
+        )
+        return component.bindProperties(newProperties)
