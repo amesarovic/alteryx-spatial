603fc5a3fc667da872868bc570fd7ffbd1d0d97d
mesarovic.andre@prophecy.io <mesarovic.andre@prophecy.io>
2025-06-24 12:20:35 +0000

    Added MakeGridTool_05 gem (clone of MakeGridTool3). Works OK.

---
+++ models/MakeGridTool_05/model_MakeGridTool_05_create_grid_miles.sql
@@ -0,0 +1,34 @@
+{{
+  config({    
+    "materialized": "table",
+    "alias": "prophecy_tmp__mcaho5rw__MakeGridTool_05__create_grid_miles",
+    "database": "andre_dev",
+    "schema": "spatial"
+  })
+}}
+
+WITH new_england AS (
+
+  SELECT * 
+  
+  FROM {{ source('andre_dev.alteryx_spatial', 'new_england') }}
+
+),
+
+create_grid_miles AS (
+
+  {{
+    andre_spatial_07.MakeGridTool_05(
+      'new_england', 
+      [{ "name": "name", "dataType": "String" }, { "name": "geometry", "dataType": "String" }], 
+      'geometry', 
+      0.1, 
+      'miles'
+    )
+  }}
+
+)
+
+SELECT *
+
+FROM create_grid_miles
