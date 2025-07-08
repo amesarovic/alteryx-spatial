1f2676de1a0692ae6dee1775c1673da2937565c5
mesarovic.andre@prophecy.io <mesarovic.andre@prophecy.io>
2025-07-08 15:07:34 +0000

    Deleted ST_BufferTool_03 macro

--- pipelines/ST_BufferTool_02.py
+++ pipelines/ST_BufferTool_02.py
@@ -13,9 +13,9 @@ with DAG(Schedule = Schedule):
           "additionalProperties": None
         }
     )
-    model_ST_BufferTool_02_buffer_new_england = Task(
-        task_id = "model_ST_BufferTool_02_buffer_new_england", 
+    model_ST_BufferTool_02_ST_BufferTool_02_1 = Task(
+        task_id = "model_ST_BufferTool_02_ST_BufferTool_02_1", 
         component = "Model", 
-        modelName = "model_ST_BufferTool_02_buffer_new_england"
+        modelName = "model_ST_BufferTool_02_ST_BufferTool_02_1"
     )
-    new_england.out >> model_ST_BufferTool_02_buffer_new_england.in_0
+    new_england.out >> model_ST_BufferTool_02_ST_BufferTool_02_1.in_0
